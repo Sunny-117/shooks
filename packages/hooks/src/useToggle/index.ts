087139ed80e664df2af6bf6f8f8f8573733dbc68
付志强 <zhiqiangfu6@gmail.com>
2022-12-27 21:30:24 +0800

    feat: useToggle

--- packages/hooks/src/useToggle/index.ts
+++ packages/hooks/src/useToggle/index.ts
@@ -1,8 +1,40 @@
-import { useState } from "react"
+import { useMemo, useState } from 'react';
 
+export interface Actions<T> {
+    setLeft: () => void;
+    setRight: () => void;
+    set: (value: T) => void;
+    toggle: () => void;
+}
 
-function useToggle() {
-    const [state, setState] = useState(defaultValue);
+function useToggle<T = boolean>(): [boolean, Actions<T>];
+
+function useToggle<T>(defaultValue: T): [T, Actions<T>];
+
+function useToggle<T, U>(defaultValue: T, reverseValue: U): [T | U, Actions<T | U>];
+
+function useToggle<D, R>(defaultValue: D = false as unknown as D, reverseValue?: R) {
+    const [state, setState] = useState<D | R>(defaultValue);
+
+    const actions = useMemo(() => {
+        const reverseValueOrigin = (reverseValue === undefined ? !defaultValue : reverseValue) as D | R;
+
+        const toggle = () => setState((s) => (s === defaultValue ? reverseValueOrigin : defaultValue));
+        const set = (value: D | R) => setState(value);
+        const setLeft = () => setState(defaultValue);
+        const setRight = () => setState(reverseValueOrigin);
+
+        return {
+            toggle,
+            set,
+            setLeft,
+            setRight,
+        };
+        // useToggle ignore value change
+        // }, [defaultValue, reverseValue]);
+    }, []);
+
     return [state, actions];
 }
-export default useToggle
+
+export default useToggle;
