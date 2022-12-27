799f324651f53ec2d68910810654b4f4617f0dce
付志强 <zhiqiangfu6@gmail.com>
2022-12-27 21:18:52 +0800

    feat: useSetState

--- packages/hooks/src/useToggle/index.ts
+++ packages/hooks/src/useToggle/index.ts
@@ -2,23 +2,7 @@ import { useState } from "react"
 
 
 function useToggle() {
-    const [state, setState] = useState('a', 'b')
-    const toggle = () => {
-
-    }
-    const setLeft = () => {
-        setState('a')
-    }
-    const setRight = () => {
-        setRight('b')
-    }
-    return [
-        state,
-        {
-            toggle,
-            setLeft,
-            setRight
-        }
-    ]
+    const [state, setState] = useState(defaultValue);
+    return [state, actions];
 }
 export default useToggle
